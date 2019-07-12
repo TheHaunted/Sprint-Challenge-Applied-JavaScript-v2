21d4a8af7462031bdaf10c27ffa2b21b2b9d6387
TheHaunted <kevin_lopez@live.com>
2019-07-12 10:38:06 -0500

    retrieved tabs component data

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,10 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(data => {
+        let tabs = data.data;
+        console.log(tabs);
+    })
+    .catch(error => console.log(error.message));
+
